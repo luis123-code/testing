8cefb5edc12997339771446cf250166526c60e03
Luis Ramirez Zevallos <118630481+luis123-code@users.noreply.github.com>
2025-06-12 16:51:34 -0500

    se modifico

--- src/__testing__/01-math.test.js
+++ src/__testing__/01-math.test.js
@@ -3,9 +3,9 @@ const sumar = require('../01-math');
 
 describe("probando github action", () => {
   test('suma 2 + 3 debe dar 5', () => {
-    expect(sumar(2, 3)).toBe(6);
+    expect(sumar(2, 3)).toBe(5);
   })
   test('suma 4 + 3 debe dar 7', () => {
-    expect(sumar(4, 3)).toBe(5);
+    expect(sumar(4, 3)).toBe(7);
   })
 })
